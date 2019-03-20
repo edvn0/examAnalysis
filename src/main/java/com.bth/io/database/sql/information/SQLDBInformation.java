49ef60a904ba2fa0fd1370810e10477068021cb4
Edwin Carlsson <edwin98carlsson@gmail.com>
2019-03-20 12:51:57 +0100

    Connecting a GUI. Added Gradle.

---
+++ src/main/java/com.bth/io/database/sql/information/SQLDBInformation.java
@@ -0,0 +1,8 @@
+package com.bth.io.database.sql.information;
+
+public class SQLDBInformation
+{
+  public static String url = "jdbc:mysql://localhost:8889/stats_exams";
+  public static String user = "Edwin";
+  public static String pass = "Edwin98";
+}
