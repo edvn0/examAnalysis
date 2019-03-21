1804933343b559a684d4427f9c5ba6b6bc827c1d
Edwin Carlsson <edwin98carlsson@gmail.com>
2019-03-21 22:33:22 +0100

    Updates and syncing.

--- src/main/java/com/bth/io/database/mongodb/information/MongoDBInformation.java
+++
@@ -1,8 +0,0 @@
-package com.bth.io.database.mongodb.information;
-
-class MongoDBInformation {
-  private static final String password = "Frostdruid98199819";
-  static String database = "JavaProjectDatabase";
-  static String collection = "KeyValuePairs";
-  static String connection = "mongodb+srv://edwin-carlsson:" + password + "@javaprojecttesting-jp1q0.mongodb.net/test?retryWrites=true";
-}
--- src/main/java/com/bth/io/database/sql/information/SQLDBInformation.java
+++
@@ -1,7 +0,0 @@
-package com.bth.io.database.sql.information;
-
-public class SQLDBInformation {
-  public static final String url = null; // "jdbc:mysql://localhost:8889/stats_exams";
-  public static final String user = null; // "Edwin";
-  public static final String pass = null; // "Edwin98";
-}
